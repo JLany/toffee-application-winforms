12161426e5b5b9d19c083e26bcd3c73df8e40edb
Kilany <y.m.elkilany@gmail.com>
2023-05-11 00:34:18 +0300

    Refresh catalog after each order

--- ToffeeApplication/PlaceOrderForm.cs
+++ ToffeeApplication/PlaceOrderForm.cs
@@ -30,20 +30,13 @@ namespace ToffeeApplication
             InitializeComponent();
 
             CurrentSession = session;
+            inventoryItems = Program.Inventory.GetItemCatalog();
 
             // set up catalog
             var catalog = this.itemCatalogListView;
             var cart = this.shoppingCartListView;
 
-            catalog.View = View.Details;
-            catalog.GridLines = true;
-            catalog.FullRowSelect = true;
-            catalog.Columns.Add("ID").Width = 40;
-            catalog.Columns.Add("Name").Width = 100;
-            catalog.Columns.Add("Description").Width = 140;
-            catalog.Columns.Add("Price").Width = 45;
-            catalog.Columns.Add("Discount").Width = 70;
-            catalog.MultiSelect = false;
+            SetUpItemCatalog();
 
             // set up shopping cart
             cart.View = View.Details;
@@ -56,7 +49,48 @@ namespace ToffeeApplication
             cart.MultiSelect = false;
 
             // populate catalog
-            inventoryItems = Program.Inventory.GetItemCatalog();
+            PopulateItemCatalog();
+            
+
+            this.addSelectedItemButton.Click += AddSelectedItemButton_Click;
+            this.removeSelectedItemButton.Click += RemoveSelectedItemButton_Click;
+            this.placeOrderButton.Click += PlaceOrderButton_Click;
+            this.FormClosed += PlaceOrderForm_FormClosed;
+        }
+
+        private void SetUpShoppingCart()
+        {
+            var cart = this.shoppingCartListView;
+
+            cart.View = View.Details;
+            cart.GridLines = true;
+            cart.FullRowSelect = true;
+            cart.Columns.Add("ID").Width = 40;
+            cart.Columns.Add("Name").Width = 100;
+            cart.Columns.Add("Sale Price").Width = 120;
+            cart.Columns.Add("Quantity").Width = 70;
+            cart.MultiSelect = false;
+        }
+
+        private void SetUpItemCatalog()
+        {
+            var catalog = this.itemCatalogListView;
+
+            catalog.View = View.Details;
+            catalog.GridLines = true;
+            catalog.FullRowSelect = true;
+            catalog.Columns.Add("ID").Width = 40;
+            catalog.Columns.Add("Name").Width = 100;
+            catalog.Columns.Add("Description").Width = 140;
+            catalog.Columns.Add("Price").Width = 45;
+            catalog.Columns.Add("Discount").Width = 70;
+            catalog.MultiSelect = false;
+        }
+
+        private void PopulateItemCatalog()
+        {
+            var catalog = this.itemCatalogListView;
+            catalog.Items.Clear();
 
             foreach (var item in inventoryItems.Values)
             {
@@ -66,11 +100,6 @@ namespace ToffeeApplication
                         new string[] { $"{item.Id}", item.Name, item.Description, $"${item.Price}", $"{item.DiscountPercentage}%" }));
                 }
             }
-
-            this.addSelectedItemButton.Click += AddSelectedItemButton_Click;
-            this.removeSelectedItemButton.Click += RemoveSelectedItemButton_Click;
-            this.placeOrderButton.Click += PlaceOrderButton_Click;
-            this.FormClosed += PlaceOrderForm_FormClosed;
         }
 
         private void PlaceOrderForm_FormClosed(object? sender, FormClosedEventArgs e)
@@ -91,11 +120,15 @@ namespace ToffeeApplication
 
             shoppingCartItems.Clear();
             PopulateShoppingCart();
+            PopulateItemCatalog();
         }
 
         private void RemoveSelectedItemButton_Click(object? sender, EventArgs e)
         {
             var cart = this.shoppingCartListView;
+
+            if (cart.SelectedItems.Count <= 0) { return; }
+
             var selectedItem = (ListViewItem)cart.SelectedItems[0].Clone();
             int id = int.Parse(selectedItem.SubItems[0].Text);
 
@@ -111,40 +144,39 @@ namespace ToffeeApplication
 
             // catalog.Focus();
 
-            if (catalog.SelectedItems.Count > 0)
+            if (catalog.SelectedItems.Count <= 0) { return; }
+            
+            var selectedItem = (ListViewItem)catalog.SelectedItems[0].Clone();
+            int id = int.Parse(selectedItem.SubItems[0].Text);
+
+            int selectedQuantity;
+            bool notEmpty = int.TryParse(this.quantityTextBox.Text, out selectedQuantity);
+            selectedQuantity = (notEmpty ? selectedQuantity : 1);
+
+            if (selectedQuantity > inventoryItems[id].AvailableQuantity)
             {
-                var selectedItem = (ListViewItem)catalog.SelectedItems[0].Clone();
-                int id = int.Parse(selectedItem.SubItems[0].Text);
-
-                int selectedQuantity;
-                bool notEmpty = int.TryParse(this.quantityTextBox.Text, out selectedQuantity);
-                selectedQuantity = (notEmpty ? selectedQuantity : 1);
-
-                if (selectedQuantity > inventoryItems[id].AvailableQuantity)
-                {
-                    // issue an error for the user, stop execution 
-                }
-
-                if (shoppingCartItems.ContainsKey(inventoryItems[id]))
-                {
-                    shoppingCartItems[inventoryItems[id]] += selectedQuantity;
-                }
-                else
-                {
-                    shoppingCartItems.Add(inventoryItems[id], selectedQuantity);
-                }
-
-                PopulateShoppingCart();
+                // issue an error for the user, stop execution 
             }
 
-            this.quantityTextBox.Clear();            
+            if (shoppingCartItems.ContainsKey(inventoryItems[id]))
+            {
+                shoppingCartItems[inventoryItems[id]] += selectedQuantity;
+            }
+            else
+            {
+                shoppingCartItems.Add(inventoryItems[id], selectedQuantity);
+            }
+
+            PopulateShoppingCart();
+
+        this.quantityTextBox.Clear();            
         }
 
         private void PopulateShoppingCart()
         {
             var cart = this.shoppingCartListView;
-
             cart.Items.Clear();
+
             foreach (var entry in shoppingCartItems)
             {
                 cart.Items.Add(
